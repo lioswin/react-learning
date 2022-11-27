48ebe4d8b87f682c5f9bb0814a269ace68181d9e
lioswin <lioswinrite@gmail.com>
2022-11-27 22:01:35 +0300

    added the clear people functionality

--- birthday/src/App.js
+++ birthday/src/App.js
@@ -6,9 +6,9 @@ function App() {
   return(
     <main>
       <section className='container'>
-        <h3>0 birthdays today</h3>
-        <List/>
-        <button onClick={()=> console.log('ypu clicked me')}>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people}/>
+        <button onClick={()=>setPeople([])}>
           Clear all
         </button>
       </section>
