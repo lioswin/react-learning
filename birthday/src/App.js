b71fc26b293f22f8525366596e7af2e5df3b3db5
lioswin <lioswinrite@gmail.com>
2022-11-27 21:50:40 +0300

    created layout

--- birthday/src/App.js
+++ birthday/src/App.js
@@ -2,7 +2,18 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people,setPeople] = useState(data);
+  return(
+    <main>
+      <section className='container'>
+        <h3>0 birthdays today</h3>
+        <List/>
+        <button onClick={()=> console.log('ypu clicked me')}>
+          Clear all
+        </button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
